8f718d2049326129ec57ad190ba2392b7f54b73c
DataZer095 <321mts95@gmail.com>
2022-06-08 00:24:03 +0300

    -exam1_upload

---
+++ 03_Exam1/Exam_Part1/exam1.py
@@ -0,0 +1,22 @@
+print("I'm a math teacher and a programmer!")
+print("3.1415")
+print('Поэма "Мёртвые души" одна из самых интересных')
+
+
+
+
+
+print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+print("Honey, what's your hurry", end='?')
+print()
+s = 13
+k = -5
+d = s + 2
+s = d
+k = 2 * s
+print(s + k + d)
+print()
+a = 17 // (23 % 7)
+b = 34 % a * 5 - 29 % 4 * 3
+print(a * b)
