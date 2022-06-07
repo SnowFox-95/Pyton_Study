95b5d1c17628c9e1c83bb9a92d8bd8621381ab07
DataZer095 <321mts95@gmail.com>
2022-06-07 15:40:16 +0300

    - 02_4_upload_1

---
+++ 02_Data-input-and-output/02_3_-sep-_and_-end-_parameters/02_3_14.py
@@ -0,0 +1,23 @@
+#Что будет напечатано в результате выполнения следующей программы?
+
+
+'''
+Задача
+ |
+ V
+'''
+
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
+
+
+'''
+Решение " Python+C#=awesome "
+
+'''
