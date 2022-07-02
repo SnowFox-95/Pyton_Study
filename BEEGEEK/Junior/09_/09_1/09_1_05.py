866d49e7693d91d535f7892130a0e946ddcd0be8
DataZer095 <321mts95@gmail.com>
2022-07-02 23:33:24 +0300

    BeeGek 09_1p2

---
+++ BEEGEEK/Junior/09_/09_1/09_1_05.py
@@ -0,0 +1,10 @@
+"""
+Дополните приведенный код, используя индексатор, так чтобы он вывел символ запятой.
+"""
+# Изначальный код
+# s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+# print(s[])
+
+# Решение
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[7])
---
+++ BEEGEEK/Junior/09_/09_1/09_1_06.py
@@ -0,0 +1,11 @@
+"""
+Дополните приведенный код, используя индексатор, так чтобы он вывел символ w.
+"""
+# Изначальный код
+# s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+# print(s[])
+
+# Решение
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+
+print(s[-10])
