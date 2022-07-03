c8ba799b5a28608dff1d82e2779f6b29e02199aa
SnowFox-95 <321mts95@gmail.com>
2022-07-03 23:58:55 +0300

    09_2

---
+++ BEEGEEK/Junior/09_String_data_type/09_2_slices/09_2_07.py
@@ -0,0 +1,12 @@
+"""
+Дополните приведенный код, используя срезы, так чтобы он вывел первые 12 символов
+строки s.
+"""
+#Исходный код
+"""
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print()
+"""
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[0:12])
---
+++ BEEGEEK/Junior/09_String_data_type/09_2_slices/09_2_09.py
@@ -0,0 +1,12 @@
+"""
+Дополните приведенный код, используя срезы, так чтобы он вывел каждый 7 символ строки
+s начиная от начала строки.
+"""
+#Исходный код
+"""
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print()
+"""
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::-1])
