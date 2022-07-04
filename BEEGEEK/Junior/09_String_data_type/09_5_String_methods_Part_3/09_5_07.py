ed97e93a97433096b4d89086f93a66e4c994f342
SnowFox-95 <321mts95@gmail.com>
2022-07-04 20:43:47 +0300

    09_6

--- BEEGEEK/Junior/09_String_data_type/09_5_String_methods_Part_3/09_5_07.py
+++ BEEGEEK/Junior/09_String_data_type/09_5_String_methods_Part_3/09_5_07.py
@@ -4,13 +4,13 @@
 
 «In 2010, someone paid 10k Bitcoin for two pizzas.» (без кавычек).
 """
-#Исходный код
+# Исходный код
 """
 s = 'In {0}, someone paid {1} {2} for two pizzas.'
 print()
 """
 year = 2010
-count ='10k'
+count = '10k'
 valute = 'Bitcoin'
-s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year,count,valute)
+s = 'In {0}, someone paid {1} {2} for two pizzas.'.format(year, count, valute)
 print(s)
