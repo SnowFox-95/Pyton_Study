ca1f3b1ec1c4a5e77f85f485a6ca1320e580e369
SnowFox-95 <321mts95@gmail.com>
2022-07-07 22:33:05 +0300

    11_5_09.py

--- BEEGEEK/Junior/11_List/11_5/11_5_09.py
+++ BEEGEEK/Junior/11_List/11_5/11_5_09.py
@@ -12,3 +12,11 @@
 Формат выходных данных
 Программа должна вывести одно число – количество пар элементов, равных друг другу.
 """
+str = input()
+list_str = str.split(' ')
+count = 0
+for i in range(len(list_str)):
+    for j in range(i + 1, len(list_str)):
+        if list_str[i] == list_str[j]:
+            count += 1
+print(count)
