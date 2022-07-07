dfa963d1c68d9f9d71f07cc2d1fca6e504a8208b
SnowFox-95 <321mts95@gmail.com>
2022-07-07 14:29:10 +0300

    11_3_08.py

--- BEEGEEK/Junior/11_/11_3/11_3_08.py
+++ BEEGEEK/Junior/11_/11_3/11_3_08.py
@@ -8,3 +8,10 @@
 
 Примечание. Последний элемент списка состоит из 26 символов z.
 """
+list_of_alphabet = []
+n = 1
+for i in range(97, 97 + 26):
+    str = chr(i) * n
+    list_of_alphabet.append(str)
+    n += 1
+print(list_of_alphabet)
