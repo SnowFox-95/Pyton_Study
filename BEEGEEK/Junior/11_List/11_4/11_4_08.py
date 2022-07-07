343226e19e802596660974b70d2b659dfbe728d2
SnowFox-95 <321mts95@gmail.com>
2022-07-07 20:47:26 +0300

    11_4_08.py

--- BEEGEEK/Junior/11_List/11_4/11_4_08.py
+++ BEEGEEK/Junior/11_List/11_4/11_4_08.py
@@ -12,3 +12,20 @@ Negatives, Zeros and Positives
 Формат выходных данных
 Программа должна вывести текст в соответствии с условием задачи.
 """
+n = int(input())
+list_of_n = []
+for i in range(n):
+    x = int(input())
+    list_of_n.append(x)
+list_of_neg = []
+list_of_zer = []
+list_of_poz = []
+for i in range(len(list_of_n)):
+    if list_of_n[i] < 0:
+        list_of_neg.append(list_of_n[i])
+    elif list_of_n[i] == 0:
+        list_of_zer.append(list_of_n[i])
+    else:
+        list_of_poz.append(list_of_n[i])
+list_of_sort_n = list_of_neg + list_of_zer + list_of_poz
+print(*list_of_sort_n,sep='\n')
