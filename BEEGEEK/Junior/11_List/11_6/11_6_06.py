0aa5cf90b624544887f01729797e4f1ed9ad0859
SnowFox-95 <321mts95@gmail.com>
2022-07-08 12:44:26 +0300

    11_6_06.py

--- BEEGEEK/Junior/11_List/11_6/11_6_06.py
+++ BEEGEEK/Junior/11_List/11_6/11_6_06.py
@@ -13,3 +13,13 @@
 
 Примечание. Используйте подходящие встроенные функции и списочные методы.
 """
+str = input()
+list_str = str.split(' ')
+for i in range(len(list_str)):
+    list_str[i] = int(list_str[i])
+min_list_str = min(list_str)
+max_list_str = max(list_str)
+max_index = list_str.index(max_list_str)
+min_index = list_str.index(min_list_str)
+list_str[min_index], list_str[max_index] = list_str[max_index], list_str[min_index]
+print(*list_str)
