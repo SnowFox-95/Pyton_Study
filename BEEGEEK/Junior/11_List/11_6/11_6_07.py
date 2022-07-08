7865fed44ba00e14d610a628731d70256a0951a0
SnowFox-95 <321mts95@gmail.com>
2022-07-08 21:19:02 +0300

    11_6_06.py

--- BEEGEEK/Junior/11_List/11_6/11_6_07.py
+++ BEEGEEK/Junior/11_List/11_6/11_6_07.py
@@ -13,3 +13,10 @@
 
 Примечание. Артикли могут начинаться с заглавной буквы 'A', 'An', 'The'.
 """
+str = input()
+count = 0
+list_str = str.lower().split(' ')
+for i in range(len(list_str)):
+    if list_str[i] == 'a' or list_str[i] == 'an' or list_str[i] == 'the':
+        count += 1
+print('Общее количество артиклей:', count)
