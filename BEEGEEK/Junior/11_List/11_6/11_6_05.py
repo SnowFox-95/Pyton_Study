ac10b601a1e606fc5d04c86a3b0a9b851e362a4b
SnowFox-95 <321mts95@gmail.com>
2022-07-08 12:27:18 +0300

    11_6_05.py

--- BEEGEEK/Junior/11_List/11_6/11_6_05.py
+++ BEEGEEK/Junior/11_List/11_6/11_6_05.py
@@ -10,3 +10,12 @@
 Вывел список, с помощью функции print()
 """
 numbers = [8, 9, 10, 11]
+numbers.pop(1)
+numbers.insert(1, 17)
+numbers.append(4)
+numbers.append(5)
+numbers.append(6)
+numbers.pop(0)
+numbers += numbers.copy()
+numbers.insert(3, 25)
+print(numbers)
