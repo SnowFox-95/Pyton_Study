70bdb2da138c4e43fd994f7499c53e1305f5041c
SnowFox-95 <321mts95@gmail.com>
2022-07-11 18:08:30 +0300

    init 13_5

---
+++ BEEGEEK/Junior/13_Functions/13_5/13_5_06.py
@@ -0,0 +1,32 @@
+"""
+Ровно в одном
+Напишите функцию is_one_away(word1, word2), которая принимает в качестве аргументов
+два слова word1 и word2 и возвращает значение True если слова имеют одинаковую длину
+  и отличаются ровно в 1 символе и False в противном случае.
+
+ Примечание. Следующий программный код:
+
+print(is_one_away('bike', 'hike'))
+print(is_one_away('water', 'wafer'))
+print(is_one_away('abcd', 'abpo'))
+print(is_one_away('abcd', 'abcde'))
+должен выводить:
+
+True
+True
+False
+False
+"""
+
+
+# объявление функции
+def is_one_away(word1, word2):
+    pass
+
+
+# считываем данные
+txt1 = input()
+txt2 = input()
+
+# вызываем функцию
+print(is_one_away(txt1, txt2))
