4ffd4e7dfacf4da75acdb6ab3b4cbb19dbaafb58
SnowFox-95 <321mts95@gmail.com>
2022-07-11 12:14:12 +0300

    init 13_2

---
+++ BEEGEEK/Junior/13_Functions/13_2/13_2_09.py
@@ -0,0 +1,21 @@
+"""
+ФИО
+Напишите функцию print_fio(name, surname, patronymic), которая принимает три параметра:
+
+name – имя человека;
+surname – фамилия человека;
+patronymic – отчество человека;
+а затем выводит на печать ФИО человека.
+
+Примечание. Предусмотрите тот факт, что все три буквы в ФИО должны иметь верхний регистр.
+"""
+
+# объявление функции
+def print_fio(name, surname, patronymic):
+    pass
+
+# считываем данные
+name, surname, patronymic = input(), input(), input()
+
+# вызываем функцию
+print_fio(name, surname, patronymic)
