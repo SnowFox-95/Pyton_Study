0fe0977d71548c87d6c318c20734bae6a076e1c1
SnowFox-95 <321mts95@gmail.com>
2022-07-11 10:55:18 +0300

    12_2_04.py

--- BEEGEEK/Junior/Exams/12_Exam5/12_2_04.py
+++ BEEGEEK/Junior/Exams/12_Exam5/12_2_04.py
@@ -18,3 +18,30 @@ abc-def-hijk или
 Примечание. Телефонный номер должен содержать только цифры и символ -, а количество
 цифр в каждой группе должны быть правильным.
 """
+number = input()
+list_len_n = [len(i) for i in number.split("-")]
+control_summ = 0
+for c in number:
+    new_number = number.replace('-', '')
+# print(new_number)
+if list_len_n == [3, 3, 4] or list_len_n == [1, 3, 3, 4]:
+    if new_number[0] == '7':
+        for c in new_number:
+            if c.isdigit():
+                control_summ += 1
+        #print(control_summ)
+        if control_summ == 11:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        for c in new_number:
+            if c.isdigit():
+                control_summ += 1
+        #print(control_summ)
+        if control_summ == 10:
+            print('YES')
+        else:
+            print('NO')
+else:
+    print('NO')
