510a4a35cf5db8fb66f9b20e25246800ff40bb94
SnowFox-95 <321mts95@gmail.com>
2022-07-11 18:12:57 +0300

    13_5_02.py

--- BEEGEEK/Junior/13_Functions/13_5/13_5_02.py
+++ BEEGEEK/Junior/13_Functions/13_5/13_5_02.py
@@ -21,7 +21,10 @@ True
 
 # объявление функции
 def is_valid_triangle(side1, side2, side3):
-    pass
+    if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+        return True
+    else:
+        return False
 
 
 # считываем данные
