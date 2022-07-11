66848e70f78d6c48b08c05a3f52c8d036cd6b525
SnowFox-95 <321mts95@gmail.com>
2022-07-11 12:23:50 +0300

    13_2_09.py

--- BEEGEEK/Junior/13_Functions/13_2/13_2_09.py
+++ BEEGEEK/Junior/13_Functions/13_2/13_2_09.py
@@ -10,12 +10,13 @@ patronymic – отчество человека;
 Примечание. Предусмотрите тот факт, что все три буквы в ФИО должны иметь верхний регистр.
 """
 
+
 # объявление функции
 def print_fio(name, surname, patronymic):
-    pass
+    print((surname[0] + name[0] + patronymic[0]).upper())
+
 
 # считываем данные
 name, surname, patronymic = input(), input(), input()
-
 # вызываем функцию
 print_fio(name, surname, patronymic)
