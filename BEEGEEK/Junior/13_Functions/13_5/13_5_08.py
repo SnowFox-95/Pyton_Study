ef576e72941a1a94d6315040bb9149e564993974
SnowFox-95 <321mts95@gmail.com>
2022-07-12 18:47:52 +0300

    13_5_08.py

--- BEEGEEK/Junior/13_Functions/13_5/13_5_08.py
+++ BEEGEEK/Junior/13_Functions/13_5/13_5_08.py
@@ -31,7 +31,56 @@ False
 
 # объявление функции
 def is_valid_password(password):
-    pass
+    list_pass = password.split(':')
+    if len(list_pass) == 3:
+        first = int((list_pass[0]))
+        second = int((list_pass[1]))
+        third = int((list_pass[2]))
+        first_check = is_palindrome(str(first))
+        second_check = is_prime(second)
+        third_check = is_even(third)
+        if first_check == True and second_check == True and third_check == True:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def is_palindrome(text):
+    clean_rev_text = text[::-1].replace(' ', '').replace('.', '').replace(',', ''). \
+        replace('!', '').replace('?', '').replace('-', '').lower()
+    clean_text = text[:].replace(' ', '').replace('.', '').replace(',', ''). \
+        replace('!', '').replace('?', '').replace('-', '').lower()
+    if len(clean_text) == len(clean_rev_text):
+        count = 0
+        for i in range(len(clean_text)):
+            if clean_text[i] == clean_rev_text[i]:
+                count += 1
+        if count == len(clean_text) and count == len(clean_rev_text):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def is_prime(num):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        return True
+    else:
+        return False
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
 
 
 # считываем данные
