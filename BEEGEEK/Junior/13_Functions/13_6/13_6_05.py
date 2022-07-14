51aa218686928ab1749ce4330037da50a0aeea91
SnowFox-95 <321mts95@gmail.com>
2022-07-14 08:25:13 +0300

    13_6_05.py

--- BEEGEEK/Junior/13_Functions/13_6/13_6_05.py
+++ BEEGEEK/Junior/13_Functions/13_6/13_6_05.py
@@ -20,13 +20,38 @@ print(solve(1, 2, 1))
 1.0 2.5
 -1.0 -1.0
 """
+from math import pow, sqrt
+
+
 # объявление функции
 def solve(a, b, c):
-    pass
+    diskr = pow(b, 2) - 4 * a * c
+    if diskr > 0:
+        x_1 = (-b + sqrt(diskr)) / (2 * a)
+        x_2 = (-b - sqrt(diskr)) / (2 * a)
+    else:
+        x_1 = -b / (2 * a)
+        x_2 = x_1
+    return min(x_1, x_2), max(x_1, x_2)
+
 
 # считываем данные
 a, b, c = int(input()), int(input()), int(input())
 
 # вызываем функцию
 x1, x2 = solve(a, b, c)
+print(x1, x2)
+
+# TESTS DONOTCOPY!
+x1, x2 = solve(1, -4, -5)
+print(x1, x2)
+x1, x2 = solve(-2, 7, -5)
+print(x1, x2)
+x1, x2 = solve(1, 2, 1)
+print(x1, x2)
+x1, x2 = solve(22, 34, -8)
+print(x1, x2)
+x1, x2 = solve(-22, 34, -8)
+print(x1, x2)
+x1, x2 = solve(5, 20, 20)
 print(x1, x2)
