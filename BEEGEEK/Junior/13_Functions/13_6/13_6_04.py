a0e3ddc70b2ba02d814e40e3ac4f9819d699d5f6
SnowFox-95 <321mts95@gmail.com>
2022-07-14 08:14:46 +0300

    13_6_04.py

--- BEEGEEK/Junior/13_Functions/13_6/13_6_04.py
+++ BEEGEEK/Junior/13_Functions/13_6/13_6_04.py
@@ -3,7 +3,7 @@
 окружности и возвращает два значения: длину окружности и площадь круга, ограниченного
 данной окружностью.
 
-Примечание 1. Длина окружности и площадь круга радиуса rr вычисляются по формулам:
+Примечание 1. Длина окружности и площадь круга радиуса r вычисляются по формулам:
 
 
 С=2πr,S=πr^2
@@ -19,14 +19,27 @@ print(get_circle(1.5))
 6.283185307179586 3.141592653589793
 9.42477796076938 7.0685834705770345
 """
+from math import pi, pow
+
 
 # объявление функции
 def get_circle(radius):
-    pass
+    circle_length = 2 * pi * radius
+    circle_square = pi * pow(radius, 2)
+    return circle_length, circle_square
+
 
 # считываем данные
 r = float(input())
 
 # вызываем функцию
 length, square = get_circle(r)
+print(length, square)
+
+# TESTS DONOTCOPY!
+length, square = get_circle(1)
+print(length, square)
+length, square = get_circle(7.2)
+print(length, square)
+length, square = get_circle(1.5)
 print(length, square)
