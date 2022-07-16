920d8d096d7ed03c45fecaa28def946868eb35df
SnowFox-95 <321mts95@gmail.com>
2022-07-16 23:04:09 +0300

    1_3_10_02 in OOP_selfEdu

--- OOP_SelfEdu/1_3/1_3ex10.py
+++ OOP_SelfEdu/1_3/1_3ex10.py
@@ -3,7 +3,11 @@ class Figure:
     color: 'red'
 
 
-fig1 = Figure
-fig1.start_pt = (10, 5)
-fig1.end_pt = (100,20)
-fig1.color = 'blue'
+fig1 = Figure()
+setattr(fig1, 'start_pt', (10, 5))
+setattr(fig1, 'end_pt', (100, 20))
+setattr(fig1, 'color', 'blue')
+
+del fig1.color
+
+print(*fig1.__dict__)
