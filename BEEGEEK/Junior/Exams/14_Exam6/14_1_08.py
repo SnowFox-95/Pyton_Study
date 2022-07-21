07e01c51f53d29eb3dd0b2469a3ef80883c28e8f
SnowFox-95 <321mts95@gmail.com>
2022-07-21 22:07:17 +0300

    init 14_1

---
+++ BEEGEEK/Junior/Exams/14_Exam6/14_1_08.py
@@ -0,0 +1,30 @@
+"""
+Искомый месяц
+Напишите функцию get_month(language, number), которая принимает на вход два аргумента
+language – язык ru или en и number – номер месяца (от 1 до 12) и возвращает название
+месяца на русском или английском языке.
+
+Примечание. Следующий программный код:
+
+print(get_month('ru', 1))
+print(get_month('ru', 12))
+print(get_month('en', 1))
+print(get_month('en', 10))
+должен выводить:
+
+январь
+декабрь
+january
+october
+"""
+
+# объявление функции
+def get_month(language, number):
+    pass
+
+# считываем данные
+lan = input()
+num = int(input())
+
+# вызываем функцию
+print(get_month(lan, num))
