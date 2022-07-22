da752bd10a61983545ebdbcdc2b756c8ecf7a748
SnowFox-95 <321mts95@gmail.com>
2022-07-22 19:35:42 +0300

    PR#4 Шифр Цезаря
    * запрос алфавита
    
    * запрос сдвига
    
    * запрос направления
    
    * запрос фразы для работы алгоритма
    
    * реализация шифрования и дешифровки с учетом заданного сдвига

--- BEEGEEK/Junior/15_PROJECTS/pr_4_ceasar/ceasar.py
+++ BEEGEEK/Junior/15_PROJECTS/pr_4_ceasar/ceasar.py
@@ -33,25 +33,56 @@ def ceasar_rot(destin, language, string, rotate):
     global EN_LANG_UP, EN_LANG_LO, RU_LANG_UP, RU_LANG_LO
     old_str = string
     rez_str = ''
-
-    if destin:  # дешифровка
+    if destin:  # дешифр
         if language:  # дешифр на английск
             for i in range(len(old_str)):
                 if string[i] in EN_LANG_UP:
-                    order = string[i].ord()
+                    order = ord(string[i])
                     new_order = order - rotate % len(EN_LANG_UP)
-
+                    rez_str = rez_str + chr(new_order)
                 elif string[i] in EN_LANG_LO:
-                    pass
+                    order = ord(string[i])
+                    new_order = order - rotate % len(EN_LANG_LO)
+                    rez_str = rez_str + chr(new_order)
                 else:
-                    continue
+                    rez_str = rez_str + string[i]
         else:  # дешифр на русск
-            pass
-    else:  # расшифровка
+            for i in range(len(old_str)):
+                if string[i] in RU_LANG_UP:
+                    order = ord(string[i])
+                    new_order = order - rotate % len(RU_LANG_UP)
+                    rez_str = rez_str + chr(new_order)
+                elif string[i] in RU_LANG_LO:
+                    order = ord(string[i])
+                    new_order = order - rotate % len(RU_LANG_LO)
+                    rez_str = rez_str + chr(new_order)
+                else:
+                    rez_str = rez_str + string[i]
+    else:  # шифр.
         if language:  # шифр на английск
-            pass
+            for i in range(len(old_str)):
+                if string[i] in EN_LANG_UP:
+                    order = ord(string[i])
+                    new_order = order + rotate % len(EN_LANG_UP)
+                    rez_str = rez_str + chr(new_order)
+                elif string[i] in EN_LANG_LO:
+                    order = ord(string[i])
+                    new_order = order + rotate % len(EN_LANG_LO)
+                    rez_str = rez_str + chr(new_order)
+                else:
+                    rez_str = rez_str + string[i]
         else:  # шифр на русск
-            pass
+            for i in range(len(old_str)):
+                if string[i] in RU_LANG_UP:
+                    order = ord(string[i])
+                    new_order = order + rotate % len(RU_LANG_UP)
+                    rez_str = rez_str + chr(new_order)
+                elif string[i] in RU_LANG_LO:
+                    order = ord(string[i])
+                    new_order = order + rotate % len(RU_LANG_LO)
+                    rez_str = rez_str + chr(new_order)
+                else:
+                    rez_str = rez_str + string[i]
 
     return rez_str
 
@@ -59,7 +90,7 @@ def ceasar_rot(destin, language, string, rotate):
 dest = dest_valid()  # по флагу True - дешифровка, по False - расшифровка
 lang = lang_valid()  # по флагу True - английский, по False - русский
 
-print('Какую фразу нужно зашифровать?')
+print('Предоставьте фразу для расшифровки/шифрования:')
 str = input()
 
 print('На сколько символов сдвигаем?')
