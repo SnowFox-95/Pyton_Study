f2e27758cc00edd0fb5c38309f200bb342deaf84
SnowFox-95 <321mts95@gmail.com>
2022-09-20 12:58:53 +0300

    1c_module 2_3_06.py

--- 1C_Math&Python/module_2_base_python/path3_massive/2_3_06.py
+++ 1C_Math&Python/module_2_base_python/path3_massive/2_3_06.py
@@ -5,9 +5,19 @@
 """
 
 
-def main():
-    pass
+def summ(l1, l2):
+    s = 0
+    for i in range(0, 3):
+        s += int(l1[i])
+    rez = str(s)
+    s = 0
+    for i in range(0, 3):
+        s += int(l2[i])
+    rez = rez + '#' + str(s)
+    return rez
 
 
 if __name__ == "__main__":
-    main()
+    list1 = input().split(' ')
+    list2 = input().split(' ')
+    print(summ(list1, list2))
