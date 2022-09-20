c034d5852edd66262efeba8db8ea36779a89f6d6
SnowFox-95 <321mts95@gmail.com>
2022-09-20 12:51:17 +0300

    1c_module 2_3_04.py

--- 1C_Math&Python/module_2_base_python/path3_massive/2_3_04.py
+++ 1C_Math&Python/module_2_base_python/path3_massive/2_3_04.py
@@ -3,9 +3,14 @@
 """
 
 
-def main():
-    pass
+def summ(l):
+    s = 0
+    for i in range(0, 3):
+        s += int(l[i])
+    return s
 
 
 if __name__ == "__main__":
-    main()
+    listing = input().split(' ')
+
+    print(summ(listing))
