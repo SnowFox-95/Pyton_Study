898bc2df14e3e92cf303fcb3712abd10890fed39
SnowFox-95 <321mts95@gmail.com>
2022-11-17 21:40:56 +0300

    04.3.09.py

--- BEEGEEK/02_Advanced/04/04_3/04_3_08.py
+++ BEEGEEK/02_Advanced/04/04_3/04_3_08.py
@@ -12,8 +12,8 @@
 
 n = int(input())
 
-for i in range (1,n+1):
+for i in range(1, n + 1):
     list = []
-    for j in range(1,n+1):
+    for j in range(1, n + 1):
         list.append(j)
     print(list)
